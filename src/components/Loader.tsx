692b409b318af2cba4ddfe25cf043c7161a6bf39
Caleb Panza <45076058+calebpanza@users.noreply.github.com>
2024-02-05 17:11:17 -0500

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
 import { BibleProject } from "../icons/BibleProject";
 
 export function Loader({ ready }) {
+  const [_dismount, _setDismount] = useState(false);
+
+  useEffect(
+    function dismountAfterReady() {
+      if (!ready) return () => null;
+
+      const timeout = setTimeout(() => {
+        _setDismount(true);
+      }, 1000);
+
+      return () => clearTimeout(timeout);
+    },
+    [ready]
+  );
+
+  if (_dismount) return null;
+
   return (
     <div className={`loader ${ready ? "finished" : "idle"}`}>
       <BibleProject size={150} />
